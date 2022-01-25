0789aa6a5ae5a17f464ad7b637ee49febc20827c
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2022-01-25 15:05:37 +0330

    Remove ToIOSafeHash as it's now part of Olive

--- Framework/System.Extensions/String.cs
+++ Framework/System.Extensions/String.cs
@@ -8,20 +8,7 @@ namespace System
     [EscapeGCop("Hard coded values are acceptable here")]
     public static partial class ZebbleExtensions
     {
-        static Dictionary<string, string> IOSafeHashes = new Dictionary<string, string>();
-        static ConcurrentDictionary<string, object> StringLocks = new ConcurrentDictionary<string, object>();
-
-        /// <summary>
-        /// Gets a SHA1 hash of this text where all characters are alpha numeric.
-        /// </summary>
-        public static string ToIOSafeHash(this string clearText)
-        {
-            if (IOSafeHashes.TryGetValue(clearText, out string result)) return result;
-
-            result = new string(clearText.CreateSHA1Hash().ToCharArray().Where(c => c.IsLetterOrDigit()).ToArray());
-            lock (IOSafeHashes) IOSafeHashes[clearText] = result;
-            return result;
-        }
+        static ConcurrentDictionary<string, object> StringLocks = new();
 
         public static object GetLockReference(this string text) => StringLocks.GetOrAdd(text, x => new object());
     }

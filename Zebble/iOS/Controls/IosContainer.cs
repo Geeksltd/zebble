224caa5d08b9f1d7efbfb9d4f5cb8660ac5770fc
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2022-02-23 13:34:48 +0330

    Remove unused code
    Also UIRuntime.Window.Subviews[0] is suspected causing crashes

--- Zebble/iOS/Controls/IosContainer.cs
+++ Zebble/iOS/Controls/IosContainer.cs
@@ -8,14 +8,6 @@ namespace Zebble.IOS
 
         public IosContainer(View view) { }
 
-        public override void TouchesMoved(Foundation.NSSet touches, UIEvent evt)
-        {
-            base.TouchesMoved(touches, evt);
-            var touch = touches.AnyObject as UITouch;
-            if (touch is null) return;
-            var location = touch.LocationInView(UIRuntime.Window.Subviews[0]);
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing && !IsDisposing)

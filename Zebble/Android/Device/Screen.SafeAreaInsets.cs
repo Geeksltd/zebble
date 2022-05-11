4ddd8fb39792a019961fab6c9ee50070728708ec
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2022-05-11 13:04:25 +0430

    A quick fix for Android 5 and lower

--- Zebble/Android/Device/Screen.SafeAreaInsets.cs
+++ Zebble/Android/Device/Screen.SafeAreaInsets.cs
@@ -8,14 +8,52 @@
         {
             public static void DoUpdateValues()
             {
-                var insets = UIRuntime.CurrentActivity?.Window?.DecorView?.RootWindowInsets;
+                if (OS.IsAtLeast(Android.OS.BuildVersionCodes.M)) ReadFromInsets();
+                else ReadFromResources();
+            }
 
+            static void ReadFromInsets()
+            {
+                var insets = UIRuntime.CurrentActivity?.Window?.DecorView?.RootWindowInsets;
                 if (insets == null) return;
 
-                DisplaySetting.TopInset = insets.SystemWindowInsetTop;
-                DisplaySetting.RightInset = insets.SystemWindowInsetRight;
-                DisplaySetting.BottomInset = insets.SystemWindowInsetBottom;
-                DisplaySetting.LeftInset = insets.SystemWindowInsetLeft;
+                Apply(
+                    top: insets.SystemWindowInsetTop,
+                    right: insets.SystemWindowInsetRight,
+                    bottom: insets.SystemWindowInsetBottom,
+                    left: insets.SystemWindowInsetLeft
+                );
+            }
+
+            static void ReadFromResources()
+            {
+                Apply(
+                    top: GetStatusBarHeight(),
+                    right: 0,
+                    bottom: GetNavigationBarHeight(),
+                    left: 0
+                );
+            }
+
+            static int GetStatusBarHeight()
+                => GetResourceValue("status_bar_height", "dimen", "android");
+
+            static int GetNavigationBarHeight()
+                => GetResourceValue("navigation_bar_height", "dimen", "android");
+
+            static int GetResourceValue(string name, string defType, string defPackage)
+            {
+                var resourceId = Resources.GetIdentifier(name, defType, defPackage);
+                if (resourceId == 0) return 0;
+                return Resources.GetDimensionPixelSize(resourceId);
+            }
+
+            static void Apply(int top, int right, int bottom, int left)
+            {
+                DisplaySetting.TopInset = top;
+                DisplaySetting.RightInset = right;
+                DisplaySetting.BottomInset = bottom;
+                DisplaySetting.LeftInset = left;
 
                 Top = Scale.ToZebble(DisplaySetting.TopInset);
                 Right = Scale.ToZebble(DisplaySetting.RightInset);

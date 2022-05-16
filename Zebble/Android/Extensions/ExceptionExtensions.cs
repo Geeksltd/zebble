81ec5f5f6ab05e96e20830588dd1d0e257bb0ef4
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2022-05-16 16:55:27 +0430

    Add the exception to NSError and Throwable extensions

---
+++ Zebble/Android/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Java.Lang;
+
+namespace System
+{
+    public static class ExceptionExtensions
+    {
+        public static Throwable Render(this Exception exception) => Throwable.FromException(exception);
+    }
+}
---
+++ Zebble/iOS/Extentions/ExceptionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Foundation;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace System
+{
+    public static class ExceptionExtensions
+    {
+        public static NSError Render(this Exception exception)
+        {
+            var crashInfo = new Dictionary<object, object>
+            {
+                [NSError.LocalizedDescriptionKey] = exception.Message,
+                ["StackTrace"] = exception.StackTrace
+            };
+
+            return new NSError(
+                new NSString(exception.GetType().FullName),
+                -1,
+                NSDictionary.FromObjectsAndKeys(
+                    crashInfo.Values.ToArray(),
+                    crashInfo.Keys.ToArray(),
+                    crashInfo.Count
+                )
+            );
+        }
+    }
+}

9aab06691bfa3bde1f917fdcf80185d488ba5437
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2022-05-25 13:04:25 +0430

    Update the layout using UI thread

--- Zebble/iOS/Device/Keyboard.cs
+++ Zebble/iOS/Device/Keyboard.cs
@@ -25,7 +25,7 @@ namespace Zebble.Device
             SoftKeyboardHeight = (float)args.FrameEnd.Height;
             await ExpandScrollerForKeyboard();
             RaiseShown();
-            Screen.UpdateLayout();
+            Thread.UI.Post(Screen.UpdateLayout);
         }
 
         static async Task ExpandScrollerForKeyboard()
@@ -76,7 +76,7 @@ namespace Zebble.Device
         {
             CollapseScrollerForKeyboard();
             RaiseHidden();
-            Screen.UpdateLayout();
+            Thread.UI.Post(Screen.UpdateLayout);
         }
 
         internal static void CollapseScrollerForKeyboard()

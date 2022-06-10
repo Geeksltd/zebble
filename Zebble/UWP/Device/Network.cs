25ca469d4958b7c95c4f496bf3a16487b5770371
Mohsen Seifi <mohsens2@outlook.com>
2022-06-10 13:00:14 +0430

    Update Network.cs
    UWP now has a more robust network manager, which matches Android and iOS implementations, eliminating extra calls to unnecessary websites, saving bandwidth for the user and improving user and developer experience.

--- Zebble/UWP/Device/Network.cs
+++ Zebble/UWP/Device/Network.cs
@@ -4,12 +4,125 @@ namespace Zebble.Device
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using Olive;
+    using Windows.Networking.Connectivity;
+    using Windows.UI.Core;
 
     partial class Network
     {
-        const string PING_URL = "https://www.google.com";
-        static Task<bool> CheckingTask;
-        static void Init() { }
+        static bool hasNetworkAccess;
+        static bool hasInternetAccess;
+        static bool isWifi;
+        static bool isMobileData;
+        static ConnectionProfile netInfo;
+        static ulong bandwidth;
+
+        static void Init() 
+        {
+            
+            NetworkStatusChanged(null);
+            NetworkInformation.NetworkStatusChanged += NetworkStatusChanged;
+
+        }
+        private static void NetworkStatusChanged(object sender)
+        {
+            netInfo = NetworkInformation.GetInternetConnectionProfile();
+            if (netInfo == null)
+            {
+                hasNetworkAccess = false;
+                hasInternetAccess = false;
+                isWifi = false;
+                isMobileData = false;
+                bandwidth=0;
+            }
+            else
+            {
+                var level = netInfo.GetNetworkConnectivityLevel();
+                UpdateConnectionLevel(level);
+                isWifi = netInfo.IsWlanConnectionProfile;
+                isMobileData = netInfo.IsWwanConnectionProfile;
+
+                var networkAdapter=netInfo.NetworkAdapter;
+                if (networkAdapter != null)
+                {
+                    bandwidth= networkAdapter.InboundMaxBitsPerSecond;
+                }
+            }
+        }
+
+        private static void UpdateConnectionLevel(NetworkConnectivityLevel level)
+        {
+            if (netInfo == null)
+            {
+                NetworkStatusChanged(null);
+            }
+
+            switch (level)
+            {
+                case NetworkConnectivityLevel.None:
+                    hasInternetAccess = false;
+                    hasNetworkAccess = false;
+                    break;
+                case NetworkConnectivityLevel.LocalAccess:
+                    hasInternetAccess = false;
+                    hasNetworkAccess = true;
+                    break;
+                case NetworkConnectivityLevel.ConstrainedInternetAccess:
+                    hasInternetAccess = true;
+                    hasNetworkAccess = true;
+                    break;
+                case NetworkConnectivityLevel.InternetAccess:
+                    hasInternetAccess = true;
+                    hasNetworkAccess = true;
+                    break;
+                default:
+                    hasInternetAccess = false;
+                    hasNetworkAccess = false;
+                    break;
+            }
+
+        }
+
+        static async Task<List<ulong>> GetBandwidthsFromDevice()
+        {
+            if (netInfo == null)
+            {
+                NetworkStatusChanged(null);
+            }
+            return new List<ulong> { bandwidth};
+        }
+
+        static IEnumerable<DeviceConnectionType> DoGetConnectionTypes()
+        {
+            if (netInfo == null)
+            {
+                NetworkStatusChanged(null);
+            }
+            if (hasNetworkAccess)
+            {
+                if (isWifi)
+                {
+                    yield return DeviceConnectionType.WiFi;
+                }
+                if (isMobileData)
+                {
+                    yield return DeviceConnectionType.Cellular;
+                }
+                yield return DeviceConnectionType.Ethernet;
+            }
+            else
+            {
+                yield return DeviceConnectionType.Other;
+            }
+        }
+
+        public static async Task<bool> IsAvailable()
+        {
+            if (netInfo==null)
+            {
+                NetworkStatusChanged(null);
+            }
+            return hasInternetAccess;
+        }
 
         static async Task<bool> Responds(string host, int timeout)
         {
@@ -28,45 +141,6 @@ namespace Zebble.Device
             }
 
             return false;
-        }
-
-        static Task<List<ulong>> GetBandwidthsFromDevice()
-        {
-            // TODO
-            throw new NotImplementedException();
-        }
-
-        static IEnumerable<DeviceConnectionType> DoGetConnectionTypes()
-        {
-            // TODO
-            yield return DeviceConnectionType.WiFi;
-        }
-
-        public static async Task<bool> IsAvailable()
-        {
-            var checking = CheckingTask;
-            if (checking != null) return await checking;
-
-            CheckingTask = checking = Task.Run(async () =>
-            {
-                try
-                {
-                    using var client = HttpClient(PING_URL, 3.Seconds());
-                    using var stream = await client.GetStreamAsync(PING_URL);
-                    return stream.CanRead;
-                }
-                catch
-                {
-                    // No logging is needed.
-                    return false;
-                }
-                finally
-                {
-                    Task.Delay(1.Minutes()).ContinueWith(t => CheckingTask = null).GetAwaiter();
-                }
-            });
-
-            return await checking;
         }
     }
 }

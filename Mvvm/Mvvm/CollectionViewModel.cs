33163e08142eb99120a8e3de83f07f63d482eb2b
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2023-10-19 16:35:52 +0330

    Add CollectionVM-compatible Get extensions

--- Mvvm/Mvvm/CollectionViewModel.cs
+++ Mvvm/Mvvm/CollectionViewModel.cs
@@ -1,5 +1,10 @@
 ﻿using Olive;
+using System;
 using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Zebble.Mvvm
 {
@@ -7,5 +12,30 @@ namespace Zebble.Mvvm
 
     public class CollectionViewModel<TViewModel> : BindableCollection<TViewModel>, ICollectionViewModel where TViewModel : ViewModel
     {
+    }
+
+    public static class BindableExtensions
+    {
+        static readonly ConcurrentDictionary<string, IBindable> Dependents = new();
+
+        public static CollectionViewModel<TViewModel> Get<TValue, TTarget, TViewModel>(this Bindable<TValue> @this, Func<TValue, IEnumerable<TTarget>> valueProvider, [CallerFilePath] string callerFile = null, [CallerLineNumber] int callerLine = 0) where TViewModel : ViewModel, IViewModelOf<TTarget>, new()
+        {
+            return @this.Get<TValue, TTarget, TViewModel>(null, valueProvider, callerFile, callerLine);
+        }
+
+        //
+        // Summary:
+        //     Returns a durable unique nested Bindable whose value remains in sync with this
+        //     instance.
+        public static CollectionViewModel<TViewModel> Get<TValue, TTarget, TViewModel>(this Bindable<TValue> @this, string uniqueIdentifier, Func<TValue, IEnumerable<TTarget>> valueProvider, [CallerFilePath] string callerFile = null, [CallerLineNumber] int callerLine = 0) where TViewModel : ViewModel, IViewModelOf<TTarget>, new()
+        {
+            string key = $"{callerFile}|{callerLine}|{uniqueIdentifier}";
+            return (CollectionViewModel<TViewModel>)Dependents.GetOrAdd(key, delegate
+            {
+                var collectionBindable = new CollectionViewModel<TViewModel>();
+                collectionBindable.Bind(@this, source => valueProvider(source).Select(v => new TViewModel().Set(x => x.Source.Set(v))).ToList());
+                return collectionBindable;
+            });
+        }
     }
 }

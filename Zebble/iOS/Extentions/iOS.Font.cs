220f61b35062374ddff7fb316e7a31d015f635c0
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2024-01-09 20:32:24 +0330

    Scale up 15%

--- Zebble/iOS/Extentions/iOS.Font.cs
+++ Zebble/iOS/Extentions/iOS.Font.cs
@@ -64,6 +64,6 @@ namespace Zebble
             else return UIFont.FromName(GetFontName(Name), EffectiveSize);
         }
 
-        static float GetScaledFontSize(float fontSize) => (float)new UIFontMetrics(UIFontTextStyle.Body.GetConstant()).GetScaledValue(fontSize * 1.2f);
+        static float GetScaledFontSize(float fontSize) => (float)new UIFontMetrics(UIFontTextStyle.Body.GetConstant()).GetScaledValue(fontSize * 1.15f);
     }
 }
